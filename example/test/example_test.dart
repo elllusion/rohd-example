0e34213a9ed6077c88f4861aaa721d5ee5a5b101
Poverty <2971929+poorjobless@users.noreply.github.com>
2022-12-27 01:33:20 +0800

    update test class

--- example/test/example_test.dart
+++ example/test/example_test.dart
@@ -1,8 +1,22 @@
-import 'package:example/example.dart';
+/// Copyright (C) 2021 Intel Corporation
+/// SPDX-License-Identifier: BSD-3-Clause
+///
+/// example_test.dart
+/// Tests to make sure that the examples don't break.
+///
+/// 2021 September 17
+/// Author: Max Korbel <max.korbel@intel.com>
+///
+import 'package:rohd/rohd.dart';
 import 'package:test/test.dart';
 
+import 'package:example/example.dart';
+import '../bin/example.dart' as counter;
+
 void main() {
-  test('Counter', () {
-    expect(Counter.Counter(), 42);
+  tearDown(Simulator.reset);
+  test('counter example', () async {
+    await counter.main(noPrint: true);
   });
 }
+
