fad78e005687674f98a07ab7d3b96deb8d450b8b
Poverty <2971929+poorjobless@users.noreply.github.com>
2022-12-27 01:34:12 +0800

    update test class

--- fir_filter/test/fir_filter_test.dart
+++ fir_filter/test/fir_filter_test.dart
@@ -1,8 +1,31 @@
-import 'package:fir_filter/fir_filter.dart';
+/// Copyright (C) 2021 Intel Corporation
+/// SPDX-License-Identifier: BSD-3-Clause
+///
+/// example_test.dart
+/// Tests to make sure that the examples don't break.
+///
+/// 2021 September 17
+/// Author: Max Korbel <max.korbel@intel.com>
+///
+import 'package:rohd/rohd.dart';
 import 'package:test/test.dart';
 
+import 'package:fir_filter/fir_filter.dart' as firFilter;
+import '../bin/fir_filter.dart' as fir_filter;
+
 void main() {
-  test('FirFilter', () {
-    expect(FirFilter.FirFilter(), 42);
+  tearDown(Simulator.reset);
+  test('fir filter example', () async {
+    await fir_filter.main(noPrint: true);
+  });
+  
+  test('fir filter test', () async {
+    const sumWidth = 8;
+    final en = Logic(name: 'en');
+    final resetB = Logic(name: 'resetB');
+    final inputVal = Logic(name: 'inputVal', width: sumWidth);
+    final clk = SimpleClockGenerator(5).clk;
+    
+    await firFilter.FirFilter(en, resetB, clk, inputVal, [0, 0, 0, 1], bitWidth: sumWidth);
   });
 }
