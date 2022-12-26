7b1f396badf22d4a1471880d86ffa5b22696925f
Poverty <2971929+poorjobless@users.noreply.github.com>
2022-12-27 01:35:05 +0800

    update test class

--- tree/test/tree_test.dart
+++ tree/test/tree_test.dart
@@ -1,8 +1,21 @@
-import 'package:tree/tree.dart';
+/// Copyright (C) 2021 Intel Corporation
+/// SPDX-License-Identifier: BSD-3-Clause
+///
+/// example_test.dart
+/// Tests to make sure that the examples don't break.
+///
+/// 2021 September 17
+/// Author: Max Korbel <max.korbel@intel.com>
+///
+import 'package:rohd/rohd.dart';
 import 'package:test/test.dart';
 
+import 'package:tree/tree.dart';
+import '../bin/tree.dart' as tree;
+
 void main() {
-  test('TreeOfTwoInputModules', () {
-    expect(TreeOfTwoInputModules.TreeOfTwoInputModules(), 42);
+  tearDown(Simulator.reset);
+  test('tree example', () async {
+    await tree.main(noPrint: true);
   });
 }
